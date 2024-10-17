a762ec74934f4cd5a52a2c78d4268c3d3d23bbac
BastienVdp <bastienpp@hotmail.com>
2024-10-17 18:26:33 +0200

    clean code

--- script.js
+++ script.js
@@ -1,32 +1,10 @@
 import Client from "./Client.js";
 
 const customer = new Client("Bastien");
-
 const solde = document.querySelector("#balance");
-
 const links = document.querySelectorAll("[data-nav]");
-
 const histories = document.querySelector("[data-history]");
-
 let notificationTimeout;
-
-links.forEach((link) => {
-  link.addEventListener("click", () => {
-    const target = link.getAttribute("data-nav");
-
-    document.querySelectorAll("section").forEach((section) => {
-      if (section.id !== "history") section.classList.add("hidden");
-    });
-
-    document.querySelector(`section#${target}`).classList.remove("hidden");
-
-    document.querySelectorAll("input#amount").forEach((input) => {
-      input.value = "";
-    });
-  });
-});
-
-const forms = document.querySelectorAll("form");
 
 const classLists = {
   history: ["flex", "divide-x", "w-full", "[&>*]:w-full"],
@@ -45,7 +23,6 @@ const formatDate = (date) => {
     "Vendredi",
     "Samedi",
   ];
-
   const dayOfWeek = daysOfWeek[date.getDay()];
   const day = date.getDate().toString().padStart(2, "0");
   const month = (date.getMonth() + 1).toString().padStart(2, "0");
@@ -56,13 +33,73 @@ const formatDate = (date) => {
   return `${dayOfWeek} ${day}/${month}/${year} à ${hours}:${minutes}`;
 };
 
+const displayNotification = (message, isError = false) => {
+  const notification = document.querySelector("#notification");
+
+  if (notificationTimeout) clearTimeout(notificationTimeout);
+
+  if (isError) {
+    notification.classList.remove(classLists.success);
+    notification.classList.add(classLists.error);
+  } else {
+    notification.classList.remove(classLists.error);
+    notification.classList.add(classLists.success);
+  }
+
+  notification.textContent = message;
+  notification.classList.remove("hidden");
+
+  notificationTimeout = setTimeout(() => {
+    notification.classList.add("hidden");
+  }, 3000);
+};
+
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    const target = link.getAttribute("data-nav");
+
+    document.querySelectorAll("section").forEach((section) => {
+      if (section.id !== "history") section.classList.add("hidden");
+    });
+
+    document.querySelector(`section#${target}`).classList.remove("hidden");
+    document
+      .querySelectorAll("input#amount")
+      .forEach((input) => (input.value = ""));
+  });
+});
+
+const updateHistory = () => {
+  histories.innerHTML = "";
+  customer.banking.operations.forEach((operation) => {
+    const history = document.querySelector(`[data-history=${operation.type}]`);
+    const div = document.createElement("div");
+    div.classList.add(...classLists.history);
+
+    const amountSpan = document.createElement("span");
+    amountSpan.classList.add(...classLists.operation);
+    amountSpan.textContent = `${operation.amount}€`;
+
+    const dateSpan = document.createElement("span");
+    dateSpan.classList.add(...classLists.operation);
+    dateSpan.textContent = formatDate(operation.date);
+
+    div.appendChild(amountSpan);
+    div.appendChild(dateSpan);
+    history.appendChild(div);
+  });
+};
+
+const forms = document.querySelectorAll("form");
+
 forms.forEach((form) => {
   form.addEventListener("submit", (e) => {
     e.preventDefault();
     const input = form.querySelector("input");
 
-    if (input.value === "")
+    if (!input.value) {
       return displayNotification("Veuillez renseigner un montant.", true);
+    }
 
     const amount = parseInt(input.value);
     const action = form.id;
@@ -86,65 +123,14 @@ forms.forEach((form) => {
     }
 
     input.value = "";
-
     solde.textContent = customer.banking.balance;
-
-    histories.innerHTML = "";
-
-    customer.banking.operations.map((operation) => {
-      let history =
-        operation.type === "deposit"
-          ? document.querySelector("[data-history=deposit]")
-          : document.querySelector("[data-history=withdrawal]");
-
-      const div = document.createElement("div");
-      div.classList.add(...classLists.history);
-
-      const amountSpan = document.createElement("span");
-
-      amountSpan.classList.add(...classLists.operation);
-      amountSpan.textContent = `${operation.amount}€`;
-
-      const dateSpan = document.createElement("span");
-      dateSpan.classList.add(...classLists.operation);
-      dateSpan.textContent = formatDate(operation.date);
-
-      div.appendChild(amountSpan);
-      div.appendChild(dateSpan);
-
-      history.appendChild(div);
-    });
+    updateHistory();
   });
 });
 
-document
-  .querySelector("#toggle-history")
-  .addEventListener("click", () =>
-    document.querySelector("#history").classList.toggle("hidden")
-  );
-
-const displayNotification = (message, isError = false) => {
-  const notification = document.querySelector("#notification");
-
-  if (notificationTimeout) {
-    clearTimeout(notificationTimeout);
-  }
-
-  if (isError) {
-    notification.classList.remove(classLists.success);
-    notification.classList.add(classLists.error);
-  } else {
-    notification.classList.remove(classLists.error);
-    notification.classList.add(classLists.success);
-  }
-
-  notification.textContent = message;
-  notification.classList.remove("hidden");
-
-  notificationTimeout = setTimeout(() => {
-    notification.classList.add("hidden");
-  }, 3000);
-};
+document.querySelector("#toggle-history").addEventListener("click", () => {
+  document.querySelector("#history").classList.toggle("hidden");
+});
 
 const getExchangeRate = async () => {
   try {
@@ -161,89 +147,3 @@ const getExchangeRate = async () => {
 };
 
 getExchangeRate();
-// // Money deposit
-// customer.deposit(200);
-
-// // Money withdrawal
-// customer.withdraw(150);
-
-// // Interest calculation
-// customer.banking.calculateInterest();
-
-// const banking = new Banking();
-
-// // Money deposit
-// banking.depositMoney(200);
-
-// // Money withdrawal
-// banking.withdrawMoney(150);
-
-// // Interest calculation
-// banking.calculateInterest();
-
-// // Display history
-// banking.displayHistory();
-
-// const interestRates = 0.03;
-// let balance = 1000;
-// let deposit = 200;
-// let withdrawal = 150;
-// const operations = [];
-
-// const depositMoney = (amount) => {
-//   balance += amount;
-//   console.log(`Vous avez déposé ${amount}€. Nouveau solde : ${balance}€`);
-//   operations.push({
-//     type: "deposit",
-//     amount: amount,
-//     date: new Date(),
-//   });
-// };
-
-// const withdrawMoney = (amount) => {
-//   if (balance >= amount) {
-//     balance -= amount;
-//     console.log(`Vous avez retiré ${amount}€. Nouveau solde : ${balance}€`);
-//     operations.push({
-//       type: "withdrawal",
-//       amount: amount,
-//       date: new Date(),
-//     });
-//   } else console.log("Solde insuffisant pour effectuer le retrait.");
-// };
-
-// const calculateInterest = () => {
-//   balance += balance * interestRates;
-//   console.log(
-//     `Intérêts annuels de ${
-//       interestRates * 100
-//     }% ajoutés. Nouveau solde : ${balance}€`
-//   );
-// };
-
-// // Money deposit
-// depositMoney(deposit);
-
-// // Money withdrawal
-// withdrawMoney(withdrawal);
-
-// // Interest calculation
-// calculateInterest();
-
-// // Simulation of several operations
-// let newDeposit = 500;
-// depositMoney(newDeposit);
-
-// let newWithDrawal = 800;
-// withdrawMoney(newWithDrawal);
-
-// calculateInterest();
-
-// // Operation history management
-// operations.map((operation) =>
-//   console.log(
-//     operation.type === "deposit"
-//       ? `Dépôt de ${operation.amount}€ effectué le ${operation.date}`
-//       : `Retrait de ${operation.amount}€ effectué le ${operation.date}`
-//   )
-// );
